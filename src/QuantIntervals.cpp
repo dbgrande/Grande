2a076934a9989c4bf331ed5a8b8b8e9c31d056f3
David Grande <dbgrande246@gmail.com>
2020-08-06 18:32:40 -0700

    Tweak panel design.

--- src/QuantIntervals.cpp
+++ src/QuantIntervals.cpp
@@ -389,7 +389,7 @@ struct QuantIntervalsWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/QuantIntervals.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 4 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 
